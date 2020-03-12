6cdd771eee0df0550427002dceefc566bc195c2c
Elisa Munoz <elisamunozes@gmail.com>
2020-03-12 20:30:19 +0000

    added exception handling

--- app.py
+++ app.py
@@ -26,11 +26,17 @@ def get_home():
 
 @app.route('/get_pattern/<pattern_id>')
 def get_pattern(pattern_id):
-    the_pattern = mongo.db.patterns.find_one({"_id": ObjectId(pattern_id)})
+    try:
+        the_pattern = mongo.db.patterns.find_one({"_id": ObjectId(pattern_id)})
+        all_categories = mongo.db.categories.find()
+        return render_template('getpattern.html', pattern=the_pattern, categories=all_categories)
 
-    all_categories = mongo.db.categories.find()
-    return render_template('getpattern.html', pattern=the_pattern, categories=all_categories)
+    except Exception as e:
+        return render_template('404.html')
 
+
+
+    
 @app.route('/add_pattern')
 def add_pattern():
     return render_template('addpattern.html', categories=mongo.db.categories.find())
@@ -43,52 +49,63 @@ def insert_pattern():
 
 @app.route('/edit_pattern/<pattern_id>')
 def edit_pattern(pattern_id):
-    the_pattern = mongo.db.patterns.find_one({"_id": ObjectId(pattern_id)})
+    try:
+        the_pattern = mongo.db.patterns.find_one({"_id": ObjectId(pattern_id)})
 
-    all_categories = mongo.db.categories.find()
-    all_difficulty = mongo.db.difficulty.find()
-    return render_template(
-        'editpattern.html',
-        pattern=the_pattern,
-        categories=all_categories,
-        difficulty=all_difficulty
-    )
+        all_categories = mongo.db.categories.find()
+        all_difficulty = mongo.db.difficulty.find()
+        return render_template(
+            'editpattern.html',
+            pattern=the_pattern,
+            categories=all_categories,
+            difficulty=all_difficulty
+        )
+    except Exception as e:
+        return render_template('404.html')
+    
 
 @app.route('/update_pattern/<pattern_id>', methods=["POST"])
 def update_pattern(pattern_id):
-    patterns = mongo.db.patterns
-    patterns.update( {'_id': ObjectId(pattern_id)}, 
-    {
-        'category_name':request.form.get('category_name'),
-        'pattern_name':request.form.get('pattern_name'),
-        'pattern_by':request.form.get('pattern_by'),
-        'pattern_yarn_weight': request.form.get('pattern_yarn_weight'),
-        'pattern_gauge':request.form.get('pattern_gauge'),
-        'pattern_needle_size':request.form.get('pattern_needle_size'),
-        'pattern_yardage':request.form.get('pattern_yardage'),
-        'pattern_size':request.form.get('pattern_size'),
-        'pattern_language':request.form.get('pattern_language'),
-        'pattern_url':request.form.get('pattern_url'),
-        'pattern_notes':request.form.get('pattern_notes'),
-        'pattern_img':request.form.get('pattern_img'),
-        'pattern_difficulty':request.form.get('pattern_difficulty')
-    })
-    return get_pattern(pattern_id)
+    try:
+        patterns = mongo.db.patterns
+        patterns.update( {'_id': ObjectId(pattern_id)}, 
+        {
+            'category_name':request.form.get('category_name'),
+            'pattern_name':request.form.get('pattern_name'),
+            'pattern_by':request.form.get('pattern_by'),
+            'pattern_yarn_weight': request.form.get('pattern_yarn_weight'),
+            'pattern_gauge':request.form.get('pattern_gauge'),
+            'pattern_needle_size':request.form.get('pattern_needle_size'),
+            'pattern_yardage':request.form.get('pattern_yardage'),
+            'pattern_size':request.form.get('pattern_size'),
+            'pattern_language':request.form.get('pattern_language'),
+            'pattern_url':request.form.get('pattern_url'),
+            'pattern_notes':request.form.get('pattern_notes'),
+            'pattern_img':request.form.get('pattern_img'),
+            'pattern_difficulty':request.form.get('pattern_difficulty')
+        })
+        return get_pattern(pattern_id)
+    except Exception as e:
+        return render_template('404.html')
+
 
 @app.route('/delete_pattern/<pattern_id>')
 def delete_pattern(pattern_id):
-    mongo.db.patterns.remove({'_id': ObjectId(pattern_id)})
-    return redirect(url_for('get_patterns'))
+    try:
+        mongo.db.patterns.remove({'_id': ObjectId(pattern_id)})
+        return redirect(url_for('get_patterns'))
+    except Exception as e:
+        return render_template('404.html')
 
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get("IP"),
-    port=int(os.environ.get("PORT")),
-    debug=True)
+# if __name__ == '__main__':
+#     app.run(host= ("IP"),
+#     port=int(os.environ.get("PORT")),
+#     debug=False)
     
-# if __name__ == '__main__': #to run locally
-#     app.run(
-#         # host='http://127.0.0.1',
-#         port=8080,
-#         debug=True
-#     )
+if __name__ == '__main__': #to run locally
+    app.run(
+        # host='http://127.0.0.1',
+        port=8080,
+        debug=False
+    )
---
+++ app/__init__.py
@@ -0,0 +1,1 @@
+from app import routes, models, errors
